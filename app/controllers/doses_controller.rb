ec173bccee090ec33b7fd214a334e264e91281e7
Julien Da Silva <julien.da.silva@hotmail.com>
2016-05-12 17:12:00 +0200

    CRUD for cocktails & doses

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,12 +1,14 @@
 class DosesController < ApplicationController
 
-  before_action :find_cocktail, only: [ :new, :create ]
+  before_action :find_cocktail, only: [ :new, :create, :destroy ]
 
   def new
     @dose = Dose.new
   end
 
   def create
+    @dose = Dose.new(dose_params)
+    @ingredients = Ingredient.all
     @dose = @cocktail.doses.build(dose_params)
     if @dose.save
       redirect_to cocktail_path(@cocktail)
@@ -15,10 +17,14 @@ class DosesController < ApplicationController
     end
   end
 
+  def destroy
+    @dose.destroy
+  end
+
 private
 
   def dose_params
-    params.require(:dose).permit(:description, :cocktail, :ingredient)
+    params.require(:dose).permit(:description, :cocktail_id, :ingredient_id)
   end
 
   def find_cocktail
