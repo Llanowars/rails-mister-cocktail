66bf63478a6c94ae8a19fe70eb7b7b4298fc43ed
Julien Da Silva <julien.da.silva@hotmail.com>
2016-11-18 14:16:51 +0100

    checkpoint

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,3 @@
 url = 'http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
 cocktails = JSON.parse(open(url).read)
-cocktails_array = cocktails["drinks"].map { |h_ingredient| h_ingredient["strIngredient1"] }
-
-cocktails_array.uniq.each do |cocktail|
-  if cocktail != ""
-    a = Ingredient.create!(name: cocktail)
-  end
-end
+cocktails_array = cocktails["drinks"].uniq.map { |ingredient| Ingredient.create!(name: ingredient) }
