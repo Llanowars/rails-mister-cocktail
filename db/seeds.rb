f27f050de195b19a031e547e6341807b70b2b10b
Julien Da Silva <julien.da.silva@hotmail.com>
2017-07-06 14:48:00 -0400

    checkpoint

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,15 @@
-# url = 'http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
-# cocktails = JSON.parse(open(url).read)
-# cocktails_array = cocktails["drinks"].uniq.map { |ingredient| Ingredient.create!(name: ingredient) }
+puts "destroying db"
+Cocktail.destroy_all
+Ingredient.destroy_all
+
+puts "seeding ingredients"
+url = 'http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+cocktails = JSON.parse(open(url).read)
+cocktails["drinks"].uniq.map do |ingredient| 
+  Ingredient.create!(name: ingredient["strIngredient1"])
+end
+
+puts "done"
 
 
 # url = "http://static.giantbomb.com/uploads/original/9/99864/2419866-nes_console_set.png"
@@ -8,7 +17,6 @@
 # cocktail.remote_photo_url = url
 # cocktail.save
 
-Cocktail.destroy_all
 
 # url = 'http://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Cocktail'
 # drinks_serialized = open(url).read
@@ -19,10 +27,10 @@ Cocktail.destroy_all
 #   cocktail.remote_photo_url = cocktail["strDrinkThumb"]
 # end
 
-c = Cocktail.new(name: "name")
-c.remote_photo_url = "http://www.thecocktaildb.com/images/media/drink/qyyvtu1468878544.jpg"
-c.save
-url = "http://static.giantbomb.com/uploads/original/9/99864/2419866-nes_console_set.png"
-cocktail = Cocktail.new(name: 'NES')
-cocktail.remote_photo_url = url
-cocktail.save
+# c = Cocktail.new(name: "name")
+# c.remote_photo_url = "http://www.thecocktaildb.com/images/media/drink/qyyvtu1468878544.jpg"
+# c.save
+# url = "http://static.giantbomb.com/uploads/original/9/99864/2419866-nes_console_set.png"
+# cocktail = Cocktail.new(name: 'NES')
+# cocktail.remote_photo_url = url
+# cocktail.save
